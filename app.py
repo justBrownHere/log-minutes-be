d564370ec456b70d3bc7b1baa5c9047248e7dcf8
justBrownHere <nvquang1808@gmail.com>
2025-02-12 23:03:35 +0700

    update

--- app.py
+++ app.py
@@ -1,13 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
 import uvicorn 
+from fastapi.middleware.cors import CORSMiddleware
+from utilize import trim_audio
+from transcription import transcribe_audio
+import os
+import datetime
+from fastapi.responses import JSONResponse
+import tempfile
 
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+@app.post("/transcribe")
+async def transcrible(audio_file: UploadFile = File(...))->str:
+    try:
+        suffix = os.path.splitext(audio_file.filename)[1] if audio_file.filename else ""
+        with tempfile.NamedTemporaryFile(delete=False, suffix = suffix) as tmp_audio:
+            content = await audio_file.read()
+            tmp_audio.write(content)
+            tmp_audio_path = tmp_audio.name
+            transcribe_text = transcribe_audio(tmp_audio_path)
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Transcription error: {str(e)}")
+    finally:
+        if 'tmp_audio_path' in locals() or os.path.exists(tmp_audio_path):
+            os.remove(tmp_audio)
+
+    return JSONResponse(
+        conntent={
+            "transcription": transcribe_text
+        })
+
 @app.get("/")
-
 async def root():
-    return {"message": "Hello World"}
-
-
+    return {"message": "Log Metting Transcription"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ utilize.py
@@ -0,0 +1,19 @@
+from pydub import AudioSegment
+
+def trim_audio(audio_file_path: str,
+                start_time: int,
+                end_time: int, 
+                output_file_path)->str:
+    
+    try:
+        audio = AudioSegment.from_wav(audio_file_path)
+        start_time_ms = start_time * 1000
+        end_time_ms = end_time * 1000
+
+        trimmed_audio = audio[start_time_ms, end_time_ms]
+        trimmed_audio.export(output_file_path, format="wav")
+
+        print(f"Trimmed audio saved to: {output_file_path}")
+
+    except Exception as e:
+        print(f"Error: {e}")
