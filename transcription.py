ddd5157e6d4ba81b993de710394a3e3139c01c3a
justBrownHere <nvquang1808@gmail.com>
2025-02-12 17:52:05 +0700

    Upload

---
+++ transcription.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from openai import OpenAI
+
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key = api_key)
+
+def transcribe_audio(audio_file: str) -> str:
+    try:
+        with open(audio_file, "rb") as file:
+            transcribe_audio = client.audio.transcription.creaete(
+                model = "whisper-1",
+                file = file
+            )
+        return transcribe_audio.text
+    except Exception as e:
+        print(f"Error: {e}")
