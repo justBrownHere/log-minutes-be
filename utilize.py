f08ab6db5072fd5b36be6b5c262d3fbdba8f5a2b
Nguyen Thi Thien Huong <agthienhuong@Nguyens-MacBook-Air.local>
2025-02-13 10:32:19 +0700

    Add convert_to_seconds function

--- utilize.py
+++ utilize.py
@@ -1,5 +1,13 @@
 from pydub import AudioSegment
 
+
+def time_to_seconds(time: str) -> int:
+    try:
+        h, m, s = time.split(':')
+        return int(h)*3600 + int(m)*60 + int(s)
+    except Exception as e:
+        print(f"Error: {e}")
+
 def trim_audio(audio_file_path: str,
                 start_time: int,
                 end_time: int, 
@@ -7,6 +15,8 @@ def trim_audio(audio_file_path: str,
     
     try:
         audio = AudioSegment.from_wav(audio_file_path)
+        start_time = time_to_seconds(start_time)
+        end_time = time_to_seconds(end_time)
         start_time_ms = start_time * 1000
         end_time_ms = end_time * 1000
 
