95ebf31d00f267a3b5244411844f1bbee2a48a07
Nguyen Thi Thien Huong <agthienhuong@Nguyens-MacBook-Air.local>
2025-02-14 15:59:19 +0700

    update

---
+++ diarization.py
@@ -0,0 +1,16 @@
+from pyannote.audio import Pipeline
+import os
+from pyannote.core import Annotation
+
+
+def diarize_audio(audio_file: str) -> Annotation:
+    try:
+        hf_auth_token = os.getenv("HF_AUTH_TOKEN")
+        pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.0", use_auth_token = hf_auth_token)
+        diarization = pipeline(audio_file)
+        return diarization
+    except Exception as e:
+        print(f"Error diarization:{e}")
+    
+
+
